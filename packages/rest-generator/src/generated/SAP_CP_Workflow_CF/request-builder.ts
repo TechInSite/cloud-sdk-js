e8d8d27704418659f398fcca81d0ac432c26ad2d
Junjie Tang <junjie.tang@sap.com>
2020-11-26 11:28:04 +0100

    generate workflow

---
+++ packages/rest-generator/src/generated/SAP_CP_Workflow_CF/request-builder.ts
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved.
+ *
+ * This is a generated file powered by the SAP Cloud SDK for JavaScript.
+ */
+import { RestRequestBuilder } from '@sap-cloud-sdk/core';
+import { SapCpWorkflowCfApi } from './open-api/api';
+import './open-api/model';
+export const SapCpWorkflowCfApiRequestBuilder = {
+    : () => new RestRequestBuilder<SapCpWorkflowCfApi, 'undefined'>(
+  SapCpWorkflowCfApi,
+  'undefined'
+)
+  };
